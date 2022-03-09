95da7079c6ee026bcdfbad6d0fe37c2b5559feea
andre <100816114+andrezinpy@users.noreply.github.com>
2022-03-08 23:06:55 -0300

    Add files via upload

---
+++ LOGINgui.py
@@ -0,0 +1,20 @@
+from asyncio import events
+from PySimpleGUI import PySimpleGUI as sg
+sg.theme('Reddit')
+layout = [
+    [sg.Text('Usuario'), sg.Input(key='usuario')],
+    [sg.Text('Senha'), sg.Input(key='senha', password_char='*')],
+    [sg.Checkbox('Salvar o login?')],
+    [sg.Button('Entrar')]
+
+]
+
+janela = sg.Window('Tela de Login', layout)
+
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'andre' and valores['senha'] == '123':
+            print('Bem vindo ao curso de Python!')
