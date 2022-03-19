82418f8430de2c4d5b630004baa99a9565214b4a
andre <100816114+andrezinpy@users.noreply.github.com>
2022-03-18 22:06:26 -0300

    Create FatorialPlus.py

---
+++ FatorialPlus.py
@@ -0,0 +1,36 @@
+print('(a)')
+print('(b)')
+
+
+def main():
+    a = int(input('a '))
+    fat = 1
+    i = 2
+    while i <= a:
+        fat = fat*i
+        i = i + 1
+
+#-----
+main()
+
+def main():
+    b = int(input('b '))
+    fat = 1
+    i = 2
+    while i <= b:
+        fat = fat*i
+        i = i + 1
+#-----
+main()
+
+
+c = a - b
+
+def main():
+    fat = 1
+    i = 2
+    while i <= c1:
+        fat = fat*i
+        i = i + 1
+#-----
+main()
